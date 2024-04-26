3183a2e6fb39d4d79e27d2504d8eb21f0f174486
Divyansh Dubey <161144721+divyansh-dubey02@users.noreply.github.com>
2024-04-26 13:53:35 +0530

    Update program1.js
    from divyansh dubey

--- program1.js
+++ program1.js
@@ -1,9 +1,35 @@
-function smallestMissingPositiveInteger(nums) {
+function smallest_missing_positive_integer(nums) {
+  let n = nums.length;
 
-
-    // Implement the function smallest_missing_positive_integer
-  
+  let j = 0;
+  for (let i = 0; i < n; i++) {
+      if (nums[i] <= 0) {
+          [nums[i], nums[j]] = [nums[j], nums[i]];
+          j++;
+      }
   }
-  
-  module.exports = smallestMissingPositiveInteger;
 
+ 
+  nums = nums.slice(j);
+
+ 
+  for (let i = 0; i < nums.length; i++) {
+      let index = Math.abs(nums[i]) - 1;
+      if (index < nums.length && nums[index] > 0) {
+          nums[index] = -nums[index];
+      }
+  }
+
+
+  for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > 0) {
+          return i + 1; 
+      }
+  }
+
+  return nums.length + 1; 
+  
+}
+console.log(smallest_missing_positive_integer([3,4,-1,1]));
+console.log(smallest_missing_positive_integer([-1, -3, 4, 2]));
+module.exports = smallest_missing_positive_integer;
