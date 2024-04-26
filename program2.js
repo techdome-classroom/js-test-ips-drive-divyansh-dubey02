b925a20b13acfcbadedfcdb8ca2690cb44694a19
Divyansh Dubey <161144721+divyansh-dubey02@users.noreply.github.com>
2024-04-26 13:54:24 +0530

    Update program2.js
    from divyansh dubey

--- program2.js
+++ program2.js
@@ -1,7 +1,27 @@
-function longestSubstring(s) {
+function longestSubsing(s) {
 
 
-    // Implementation of longestSubstring function
+    let maxLength = 0;
+    let start = 0;
+    let charMap = {};
+
+    for (let end = 0; end < s.length; end++) {
+        const char = s[end];
+
+        if (charMap[char] !== undefined && charMap[char] >= start) {
+            start = charMap[char] + 1;
+        }
+
+        charMap[char] = end;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+
+    return maxLength;
 }
 
-module.exports = { longestSubstring };
+
+const inputsing = "abcabcbb";
+console.log("Length of longest subsing without repeating characters:", longestSubsing(inputsing)); 
+
+
+module.exports = { longestSubsing };
